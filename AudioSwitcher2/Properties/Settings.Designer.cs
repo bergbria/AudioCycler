43ed356f9c9dca7ea4146fc72f185863f2910b37
Brian Berger <bergbria@gmail.com>
2014-06-30 18:08:51 -0600

    added incomplete settings file changes that are required for compilation in earlier commits.

--- AudioSwitcher2/Properties/Settings.Designer.cs
+++ AudioSwitcher2/Properties/Settings.Designer.cs
@@ -8,22 +8,33 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AudioSwitcher2.Properties
-{
-
-
+namespace AudioSwitcher2.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
+            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
+            "tring>{0.0.0.00000000}.{de3b7243-233c-4c98-8f8f-683eb379b3a9}</string>\r\n</ArrayO" +
+            "fString>")]
+        public global::System.Collections.Specialized.StringCollection excludedDeviceIds {
+            get {
+                return ((global::System.Collections.Specialized.StringCollection)(this["excludedDeviceIds"]));
+            }
+            set {
+                this["excludedDeviceIds"] = value;
             }
         }
     }
