55d2c63c94f7d25bf703a00ce7fe4091acb12b0c
Brian Berger <bergbria@gmail.com>
2015-01-11 01:42:54 -0800

    Cleaned up whitespace

--- AudioInterface/AudioDeviceInfo.cpp
+++ AudioInterface/AudioDeviceInfo.cpp
@@ -12,30 +12,30 @@ AudioDeviceInfo::AudioDeviceInfo() {
 
 AudioDeviceInfo::AudioDeviceInfo(const wstring & deviceName, const wstring & deviceID, const DWORD& deviceState)
 {
-	this->Name = gcnew String(deviceName.c_str());
-	this->DeviceId = gcnew String(deviceID.c_str());
-	this->Status = DetermineDeviceStatus(deviceState);
+    this->Name = gcnew String(deviceName.c_str());
+    this->DeviceId = gcnew String(deviceID.c_str());
+    this->Status = DetermineDeviceStatus(deviceState);
 }
 
 String^ AudioDeviceInfo::ToString() {
-	return this->Name;
+    return this->Name;
 }
 
 DeviceStatus AudioDeviceInfo::DetermineDeviceStatus(const DWORD& deviceStatus) {
 
-	DeviceStatus status;
+    DeviceStatus status;
 
-	if (deviceStatus & DEVICE_STATE_ACTIVE) {
-		status = status | DeviceStatus::Active;
-	}
-	if (deviceStatus & DEVICE_STATE_DISABLED) {
-		status = status | DeviceStatus::Disabled;
-	}
-	if (deviceStatus & DEVICE_STATE_UNPLUGGED) {
-		status = status | DeviceStatus::Unplugged;
-	}
-	if (deviceStatus & DEVICE_STATE_NOTPRESENT) {
-		status = status | DeviceStatus::NotPresent;
-	}
-	return status;
+    if (deviceStatus & DEVICE_STATE_ACTIVE) {
+        status = status | DeviceStatus::Active;
+    }
+    if (deviceStatus & DEVICE_STATE_DISABLED) {
+        status = status | DeviceStatus::Disabled;
+    }
+    if (deviceStatus & DEVICE_STATE_UNPLUGGED) {
+        status = status | DeviceStatus::Unplugged;
+    }
+    if (deviceStatus & DEVICE_STATE_NOTPRESENT) {
+        status = status | DeviceStatus::NotPresent;
+    }
+    return status;
 }
